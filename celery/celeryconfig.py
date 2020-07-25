ae64c27cb8d95152e8f7d079532d73c4003cda9a
tipu <djaffry@gmail.com>
2020-07-25 10:46:00 -0700

    scheduled the parsers to run every minute

--- celery/celeryconfig.py
+++ celery/celeryconfig.py
@@ -1,2 +1,11 @@
+from celery.schedules import crontab
+
 broker_url = 'redis://localhost:6379/1'
 imports = ('importer',)
+
+beat_schedule = {
+    'warehouse-parser': {
+        'task': 'importer.import_data',
+        'schedule': crontab(),
+    },
+}
