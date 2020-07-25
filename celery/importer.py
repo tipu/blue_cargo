825d55e22e272552b66bbae989bd925826babe76
tipu <djaffry@gmail.com>
2020-07-25 10:45:38 -0700

    celery workers correctly utilizing parsers

--- celery/importer.py
+++ celery/importer.py
@@ -1,22 +1,17 @@
 from celery import Celery
 import celeryconfig
+from parsers import whcorp, american_storage
 
 app = Celery()
 app.config_from_object('celeryconfig')
 
-def american_storage_parser():
-    url = 'https://bluecargo.julink.fr/site2/index.html'
-
-def whcorp_parser():
-    url = 'https://bluecargo.julink.fr/site1/index.html'
-
-warehouses = [
-    american_storage_parser,
-    whcorp_parser,
-]
+parsers = [whcorp, american_storage]
 @app.task
 def import_data():
-    for warehouse_details in warehouses:
-        print(warehouse_details)
+    for parser in parsers:
+        if not hasattr(parser, 'run'):
+            raise ValueError('parser file must define a `run` function')
+        result = parser.run()
+        print(result)
 
 
